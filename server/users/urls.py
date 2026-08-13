ed0e4b791683907ef3052e36194f3c80560b9e5c
Nikita Klimchuk <benggein05@gmail.com>
2026-08-14 00:02:26 +0300

    feat: add user registration

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import LogoutView
+from .views import LogoutView, RegisterView
 
 urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- server/users/views.py
+++ server/users/views.py
@@ -1,11 +1,23 @@
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .exceptions import InvalidTokenException
+from .serializers import RegisterSerializer, UserSerializer
+from .services import UserService
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = UserService().register(**serializer.validated_data)
+        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
 
 
 class LogoutView(APIView):
