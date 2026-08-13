48d72e2f9d3370d44d8d19602385a586814821ff
Nikita Klimchuk <benggein05@gmail.com>
2026-08-13 22:28:22 +0300

    chore: add docker-compose skeleton

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -8,7 +8,7 @@ pymysql.install_as_MySQLdb()
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env = environ.Env()
-environ.Env.read_env(BASE_DIR / ".env")
+environ.Env.read_env(BASE_DIR.parent / ".env")
 
 SECRET_KEY = env("SECRET_KEY")
 
