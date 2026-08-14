5a3248b6a3edc088225394aee655dfd89d12bb82
Nikita Klimchuk <benggein05@gmail.com>
2026-08-14 12:44:58 +0300

    fix: handle unique-constraint race on registration

--- server/users/services.py
+++ server/users/services.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError as DjangoValidationError
+from django.db import IntegrityError
 
 from core.exceptions import ConflictException, ValidationException
 
@@ -11,9 +12,14 @@ class UserService:
         self._ensure_username_available(username)
         self._ensure_email_available(email)
         self._validate_password(password)
-        return User.objects.create_user(
-            username=username, email=email, password=password
-        )
+        try:
+            return User.objects.create_user(
+                username=username, email=email, password=password
+            )
+        except IntegrityError:
+            raise ConflictException(
+                message="A user with these credentials already exists."
+            )
 
     def _ensure_username_available(self, username: str) -> None:
         if User.objects.filter(username=username).exists():
