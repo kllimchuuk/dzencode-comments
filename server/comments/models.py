8b2d7471c76525a3d75298f3b39b42c10c22f795
Nikita Klimchuk <benggein05@gmail.com>
2026-08-15 01:18:15 +0300

    refactor(comments): extract user_name validator to shared constant

--- server/comments/models.py
+++ server/comments/models.py
@@ -6,6 +6,8 @@ from tree_queries.query import TreeQuerySet
 
 from core.models import TimeStampedModel
 
+USER_NAME_VALIDATOR = RegexValidator(r"^[A-Za-z0-9]+$")
+
 
 class CommentQuerySet(TreeQuerySet):
     def roots(self):
@@ -29,7 +31,7 @@ class Comment(TimeStampedModel):
     )
     user_name = models.CharField(
         max_length=60,
-        validators=[RegexValidator(r"^[A-Za-z0-9]+$")],
+        validators=[USER_NAME_VALIDATOR],
     )
     email = models.EmailField()
     home_page = models.URLField(blank=True)
